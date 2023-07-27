a09ba802655255afd896f877bf6d850b72493a4a
Mehemmed0101 <mahammadag@code.edu.az>
2023-07-27 18:10:12 +0400

    first commit

---
+++ Controllers/StudentsControllers.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApp1.Models;
+
+namespace WebApp.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private List<Student> _students = new List<Student>
+            {
+                new Student
+                {
+                    Id= 1,
+                    Firstname = "Mehemmed",
+                    Lastname = "Qurbanov",
+                    Age = 25,
+                },
+
+                new Student
+                {
+                    Id= 2,
+                    Firstname = "Ayxan",
+                    Lastname = "Bayramov",
+                    Age = 18,
+                },
+
+                new Student
+                {
+                    Id= 3,
+                    Firstname = "Rauf",
+                    Lastname = "Mehraliyev",
+                    Age = 18,
+                },
+            };
+
+
+        public IActionResult Index()
+        {
+            return View(_students);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var student = _students.FirstOrDefault(x => x.Id == id);
+
+            if (student == null)
+                return NotFound();
+
+            return View(student);
+        }
+    }
+}
